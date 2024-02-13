1ecfb336d61f10b3acdbb663241cd7110550c59c
LittleCharles <luis.carlos.vieira@live.com>
2024-02-12 22:19:53 -0300

    feat: add auth provider

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,20 +1,22 @@
+"use client";
+
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
 
 const header = () => {
-    return (  
-        <Card>
-         <CardContent className="p-5 justify-between items-center flex ">
-            <Image src="/logo.png" alt="Logo" height={22} width={120}/>
-            <Button variant="outline" size="icon">
+  return (
+    <Card>
+      <CardContent className="p-5 justify-between items-center flex ">
+        <Image src="/logo.png" alt="Logo" height={22} width={120} />
+        <Button variant="outline" size="icon">
                 <MenuIcon size={18}/>
             </Button>
-         </CardContent>
-        </Card>
+        
+      </CardContent>
+    </Card>
+  );
+};
 
-    );
-}
- 
 export default header;
